c58f28d92855cf25cc00c73209331c0a401a4882
Falco Bentvelsen <falco.bentvelsen@knmi.nl>
2025-04-03 14:46:52 +0200

    wip m92 modularization, see the course materials for solution

--- m92_challenge_modularization.py
+++ m92_challenge_modularization.py
@@ -1,3 +1,17 @@
+def celcius_2_kelvin(celsius: float) -> tuple[float, float]:
+    # Convert Celsius to Kelvin
+    kelvin = celsius + 273.15
+    if kelvin < 0:
+        raise ValueError(f"Invalid temperature, below absolute zero: kelvin {kelvin} < 0")
+    else:
+        fahrenheit = (celsius * (9 / 5)) + 32
+        if fahrenheit < -459.67:
+            raise ValueError(f"Invalid temperature, below absolute zero: fahrenheit {fahrenheit} < -459.67")
+    return celsius, kelvin
+
+# write a test for the function
+# TODO...
+
 def convert_temperature(temperature, unit):
     if unit == "F":
         # Convert Fahrenheit to Celsius
@@ -6,18 +20,7 @@ def convert_temperature(temperature, unit):
             # Invalid temperature, below absolute zero
             return "Invalid temperature"
         else:
-            # Convert Celsius to Kelvin
-            kelvin = celsius + 273.15
-            if kelvin < 0:
-                # Invalid temperature, below absolute zero
-                return "Invalid temperature"
-            else:
-                fahrenheit = (celsius * (9 / 5)) + 32
-                if fahrenheit < -459.67:
-                    # Invalid temperature, below absolute zero
-                    return "Invalid temperature"
-                else:
-                    return celsius, kelvin
+            celcius_2_kelvin(temperature)
     elif unit == "C":
         # Convert Celsius to Fahrenheit
         fahrenheit = (temperature * (9 / 5)) + 32
